cf4182f58b7e821d9b18505ca1fcb9482b221dae
marcobuontempo <marco.buon36@gmail.com>
2025-02-21 00:45:40 +1100

    Add custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import Footer from './components/Footer'
 import Gallery from './components/Gallery'
+import AnimatedCursor from 'react-animated-cursor'
 
 function App() {
 
   return (
     <>
+      <AnimatedCursor
+        color='150, 121, 105'
+        outerSize={32}
+        outerScale={2}
+      />
       <header>
         <div className='mocssa-title'>
           <h1>
             Museum of CSS Art
           </h1>
           <img className='mocssa-logo' src='/svg/landmark.svg' alt='museum logo' height={68} width={68} />
-          <p className='mocssa-note'>Note: use the latest version of Chrome for the best experience!</p>
+          <p className='mocssa-note'>note: use the latest version of Chrome for the best experience!</p>
         </div>
       </header>
 
