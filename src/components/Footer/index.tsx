1d13466942e8eae041a5de4f41c236693d933977
marcobuontempo <marco.buon36@gmail.com>
2025-06-03 17:59:30 +1000

    Minor refactor, update robots.txt

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; MoCSSA</p>
+      <p className={styles.copyright}>&copy; MoCSSA</p>
       <div>
         <a className={styles.links} href='https://github.com/marcobuontempo/mocssa/tree/main' target='_blank' rel='noreferrer'>GitHub</a>
         <a className={styles.links} href='https://github.com/marcobuontempo/mocssa/blob/main/CONTRIBUTING.md' target='_blank' rel='noreferrer'>Contribute</a>
