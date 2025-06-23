f61b907d74f0b633c116691dc23b1fdd34a2399c
marcobuontempo <marco.buon36@gmail.com>
2025-06-24 02:46:41 +1000

    Fix dynamic <head> tags

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ import EmbedContainer from "./components/EmbedContainer/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <title>Museum of CSS Art</title>
+    <meta name="description" content="MoCSSA - the Museum of CSS Art. Explore a curated gallery of creative CSS artworks from contributors worldwide, showcasing innovative web design techniques and inspiring visual art. Join the community and contribute your own CSS creations!" />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
